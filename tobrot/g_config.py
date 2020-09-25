a0a93791b1512a4a8e490ab89a5e7fa28b7c305d
Hyper-hi <63490949+Hyper-hi@users.noreply.github.com>
2020-09-26 04:13:34 +0530

    Update g_config.py

--- tobrot/g_config.py
+++ tobrot/g_config.py
@@ -10,5 +10,4 @@ class Config(Config):
 	DESTINATION_FOLDER = "Hyper" #Name of your folder read readme
 	RCLONE_CONFIG = """type = drive\nscope = drive\ntoken = {"access_token":"ya29.a0AfH6SMCWzs9D4vZLS0fwbmCw8orDJneUvMwErTJiNjSA-jRSFNsoBab6YdFQ85Xs1Ajb1OL-irW8qNX4G_F-5i6Nk_7imdA99h5KXDVaHYtsMbRo_KHTMDGmdgNrc-YGpc16M2YPW8pmmIo_z9rCQJ4q1ggMA_XCfgg","token_type":"Bearer","refresh_token":"1//0g4wnAU2VEe4KCgYIARAAGBASNwF-L9Irh2q2b8tgCdm2IeMBJ_pQ6cWn4LZsiB0VHKW2EvA0n_ngpkYp7BDyl9WxA8lWXXXCYLk","expiry":"2020-07-22T12:42:42.6241114+05:30"}
 team_drive = 0AKvqrGm6XBF_Uk9PVA"""
-	#fill taking reference of this config, dont remove """ from both side of the RCLONE_CONFIG variable 
-	INDEX_LINK = os.environ.get("INDEX_LINK", "http://a.mera.workers.dev")
+	#fill taking reference of this config, dont remove """ from both side of the RCLONE_CONFIG variable
