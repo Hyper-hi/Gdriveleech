42952f608d103b1bf3f271989c252a1987b3c216
Hyper-hi <63490949+Hyper-hi@users.noreply.github.com>
2020-09-26 03:14:36 +0530

    Update sample_config.py

--- tobrot/sample_config.py
+++ tobrot/sample_config.py
@@ -2,14 +2,14 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "1243895049:AAEAMCBi-STQqjKzzpxjP5EEvV6WCl9XLFQ")
     # The Telegram API things
-    APP_ID = int(os.environ.get("APP_ID", 12345))
-    API_HASH = os.environ.get("API_HASH")
-    OWNER_ID = int(os.environ.get("OWNER_ID", 12345))
+    APP_ID = int(os.environ.get("APP_ID", 1314600))
+    API_HASH = os.environ.get("API_HASH", a9caac8a771a5e27f617f1822288d753)
+    OWNER_ID = int(os.environ.get("OWNER_ID", 693989702))
     # Get these values from my.telegram.org
     # to store the channel ID who are authorized to use the bot
-    AUTH_CHANNEL = set(int(x) for x in os.environ.get("AUTH_CHANNEL", "").split())
+    AUTH_CHANNEL = set(int(x) for x in os.environ.get("AUTH_CHANNEL", "-1001128611435").split())
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     # Telegram maximum file upload size
@@ -37,10 +37,13 @@ class Config(object):
     CUSTOM_FILE_NAME = os.environ.get("CUSTOM_FILE_NAME", "")
     LEECH_COMMAND = os.environ.get("LEECH_COMMAND", "leech")
     YTDL_COMMAND = os.environ.get("YTDL_COMMAND", "ytdl")
-    RCLONE_CONFIG = os.environ.get("RCLONE_CONFIG", "")
-    DESTINATION_FOLDER = os.environ.get("DESTINATION_FOLDER", "TorrentLeech-Gdrive")
+    RCLONE_CONFIG = os.environ.get("RCLONE_CONFIG", "type = drive
+scope = drive
+token = {"access_token":"ya29.a0AfH6SMCWzs9D4vZLS0fwbmCw8orDJneUvMwErTJiNjSA-jRSFNsoBab6YdFQ85Xs1Ajb1OL-irW8qNX4G_F-5i6Nk_7imdA99h5KXDVaHYtsMbRo_KHTMDGmdgNrc-YGpc16M2YPW8pmmIo_z9rCQJ4q1ggMA_XCfgg","token_type":"Bearer","refresh_token":"1//0g4wnAU2VEe4KCgYIARAAGBASNwF-L9Irh2q2b8tgCdm2IeMBJ_pQ6cWn4LZsiB0VHKW2EvA0n_ngpkYp7BDyl9WxA8lWXXXCYLk","expiry":"2020-07-22T12:42:42.6241114+05:30"}
+team_drive = 0AKvqrGm6XBF_Uk9PVA")
+    DESTINATION_FOLDER = os.environ.get("DESTINATION_FOLDER", "My")
     GLEECH_COMMAND = os.environ.get("GLEECH_COMMAND", "gleech")
-    INDEX_LINK = os.environ.get("INDEX_LINK", "")
+    INDEX_LINK = os.environ.get("INDEX_LINK", "http://a.mera.workers.dev")
     TELEGRAM_LEECH_COMMAND_G = os.environ.get("TELEGRAM_LEECH_COMMAND_G", "tleech")
     CANCEL_COMMAND_G = os.environ.get("CANCEL_COMMAND_G", "cancel")
     GET_SIZE_G = os.environ.get("GET_SIZE_G", "getsize")
