93b2a57467d42256f5bf0b91d1c5a383bb1d2a19
Hyper-hi <63490949+Hyper-hi@users.noreply.github.com>
2020-09-30 01:47:04 +0530

    Update g_config.py

--- tobrot/g_config.py
+++ tobrot/g_config.py
@@ -2,12 +2,11 @@ from tobrot.sample_config import Config
 #Fill your all data, read readme for reference
 
 class Config(Config):
-	TG_BOT_TOKEN= "1243895049:AAEAMCBi-STQqjKzzpxjP5EEvV6WCl9XLFQ"
-	APP_ID = 1314600
-	API_HASH = "a9caac8a771a5e27f617f1822288d753"
-	OWNER_ID = "693989702" #ID of bot owner
-	AUTH_CHANNEL = [-1001128611435, 693989702]
-	DESTINATION_FOLDER = "Hyper" #Name of your folder read readme
-	RCLONE_CONFIG = """type = drive\nscope = drive\ntoken = {"access_token":"ya29.a0AfH6SMCWzs9D4vZLS0fwbmCw8orDJneUvMwErTJiNjSA-jRSFNsoBab6YdFQ85Xs1Ajb1OL-irW8qNX4G_F-5i6Nk_7imdA99h5KXDVaHYtsMbRo_KHTMDGmdgNrc-YGpc16M2YPW8pmmIo_z9rCQJ4q1ggMA_XCfgg","token_type":"Bearer","refresh_token":"1//0g4wnAU2VEe4KCgYIARAAGBASNwF-L9Irh2q2b8tgCdm2IeMBJ_pQ6cWn4LZsiB0VHKW2EvA0n_ngpkYp7BDyl9WxA8lWXXXCYLk","expiry":"2020-07-22T12:42:42.6241114+05:30"}
-team_drive = 0AKvqrGm6XBF_Uk9PVA"""
-	#fill taking reference of this config, dont remove """ from both side of the RCLONE_CONFIG variable
+    TG_BOT_TOKEN= "1268fill---your----data"
+    APP_ID = 1373938
+    API_HASH = "fill--your--data"
+    OWNER_ID = 12537936
+    AUTH_CHANNEL = [-10082786282972]
+    DESTINATION_FOLDER = "HelloMan" #Name of your folder read readme
+    RCLONE_CONFIG = """type = drive\nscope = drive\ntoken = {"access_token":"ya29.fill---your---data----7gGcxnYr71RBuoMVlzhAM_-fil your davxrOpIuvZ-S5lo4YzhYqP:"Bearer","refresh_token":"1//0gnh0j3fUzRITCgYIARAAGBASNwF-L9IrbbYT1QkJg6UCEo3AksHDp6DqUBrhcTBIwUWtlgmbnBQPZKvSAEOTl098VTt8zEojRnY","expiry":"2fill--your--data-07-13T10:4fillyourdata758376Z"}"""
+    #fill taking reference of this config, dont remove """ from both side of the RCLONE_CONFIG variable 
